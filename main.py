18d7b82448e911fa16029ca31387f61c3661723c
tomer.goldschmidt <tomerfry@gmail.com>
2023-06-17 13:32:33 +0300

    Add sctokenizer.

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 #! /usr/bin/python3
 
 import sys
-import capstone
 import r2pipe
-from elftools.elf.elffile import ELFFile
-
-
-def extract_text(p):
-    text_data = b''
-    with open(p) as f:
-        e = ELFFile(f)
-        text_segment = e.get_section_by_name('.text')
-        text_data = text_segment.data()
-        text_addr = text_segment['sh_addr']
-    return text_data, text_addr
+import sctokenizer
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print('Invalid argument count!')
         return -1
 
     binary_path = sys.argv[1]
+    source_path = sys.argv[2]
+
     r2 = r2pipe.open(binary_path)
     r2.cmd('aaa')
-    functions = r2.cmdj('aflj')
+    binary_funcs = r2.cmdj('aflj')
 
-    print(functions)
+    tokens = sctokenizer.tokenize_file(filepath=source_path)
+    print(tokens)
+    print(binary_funcs)
+
     r2.quit()
-
-
-
-
-
 
 
 if __name__ == '__main__':
